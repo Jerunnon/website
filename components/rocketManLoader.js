fd5fcad0c1b34c38671fe4f9cd6c563024d017c6
Jerunnon <simon-info@gmx.net>
2022-03-18 18:48:09 +0100

    working glb - not rendered correctly

--- components/rocketManLoader.js
+++ components/rocketManLoader.js
@@ -1,7 +1,38 @@
+import { forwardRef } from 'react'
+import { Box, Spinner } from '@chakra-ui/react'
 
+export const RocketManSpinner = () => (
+  <Spinner
+    size="xl"
+    position="absolute"
+    left="50%"
+    top="50%"
+    ml="calc(0px - var(--spinner-size) / 2)"
+    mt="calc(0px - var(--spinner-size))"
+  />
+)
 
-export default function rocketManLoader() {
+export const RocketManContainer = forwardRef(({ children }, ref) => (
+  <Box
+    ref={ref}
+    className="rocket-man"
+    m="auto"
+    mt={['-20px', '-60px', '-120px']}
+    mb={['-40px', '-140px', '-200px']}
+    w={[280, 480, 640]}
+    h={[280, 480, 640]}
+    position="relative"
+  >
+    {children}
+  </Box>
+))
+
+const Loader = () => {
   return (
-    <div>rocketManLoader</div>
+    <RocketManContainer>
+      <RocketManSpinner />
+    </RocketManContainer>
   )
 }
+
+export default Loader
