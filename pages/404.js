594ef3b3467660cebe05ea609ca5861f70cd3e64
Jerunnon <simon-info@gmx.net>
2022-04-02 09:31:56 +0200

    initial file

---
+++ pages/404.js
@@ -0,0 +1,27 @@
+import NextLink from 'next/link'
+import {
+  Box,
+  Heading,
+  Text,
+  Container,
+  Divider,
+  Button
+} from '@chakra-ui/react'
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Heading as="h1">Not found</Heading>
+      <Text>Die Seite konnte leider nicht gefunden werden</Text>
+      <Divider my={6} />
+      <Box my={6} align="center">
+        <NextLink href="/">
+          <Button colorScheme="teal">Zurück zur Startseite</Button>
+        </NextLink>
+      </Box>
+    </Container>
+  )
+}
+
+export default NotFound;
+export { getServerSideProps } from '../components/chakra'
